484ed97ce37897b12493a0cc03a36a93532a5757
Melissa Genger <melissa.genger@accolade.com>
2023-02-20 22:14:31 +0100

    chore: add Contentful env

---
+++ packages/ui/contentful/contentfulConfiguration.ts
@@ -0,0 +1,3 @@
+const spaceId = process.env.CONTENTFUL_SPACE_ID;
+const contentfulAccessToken = process.env.CONTENTFUL_ACCESS_TOKEN;
+const contentfulPreviewAccessPreview = process.env.CONTENTFUL_PREVIEW_ACCESS_TOKEN;
